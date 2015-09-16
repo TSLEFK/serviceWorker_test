e8dab6660db3c7e1deb113347eb44dddf576105f
TSLEFK <hephaistos@Hephastoss-Mac.local>
2015-09-16 15:35:51 +0900

    sw.js change3

--- sw.js
+++ sw.js
@@ -24,23 +24,18 @@ addEventListener("install", function(event)
 //公式からのソース
 var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
-  './',
-  './index.html',
-  'image_src.png',
-  'style.css'
+  './'
 ];
 
 self.addEventListener('install', function(event) {
-
-debugger;
  // インストール処理
- event.waitUntil(
-   caches.open(CACHE_NAME)
-     .then(function(cache) {
-       console.log('Opened cache');
-       return cache.addAll(urlsToCache);
-     })
- );
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
 
